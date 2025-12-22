f447bd513c641e1213c005959846317ef647e4ab
sucharita4867 <sucharitasardar7@gmail.com>
2025-12-22 21:50:33 +0530

    delete 2 line in this project footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,8 @@ const Footer = () => {
     <footer className="footer footer-horizontal footer-center   rounded p-10">
       <nav className="grid grid-flow-col gap-4">
         <a className="link link-hover">About us</a>
-        <a className="link link-hover">About us</a>
         <a className="link link-hover">Contact</a>
         <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
       </nav>
       <nav>
         <div className="grid grid-flow-col gap-4">
