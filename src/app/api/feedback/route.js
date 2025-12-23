9fb7b322cd2d4266398d1f4c12bc9b91b51fb473
sucharita4867 <sucharitasardar7@gmail.com>
2025-12-23 23:16:45 +0530

    create route and apis

---
+++ src/app/api/feedback/route.js
@@ -0,0 +1,25 @@
+import { feedback } from "../route";
+
+export async function GET(request) {
+  return Response.json(feedback);
+}
+
+export async function POST(request) {
+  const { message } = await request.json();
+  //   const data = await request.json();
+
+  if (!message || typeof message !== "string") {
+    return Response.json({
+      status: 400,
+      message: "please send a message",
+    });
+  }
+
+  const newFeedback = { message, id: feedback.length + 1 };
+  feedback.push(newFeedback);
+
+  return Response.json({
+    acknowledged: true,
+    insertedId: newFeedback.id,
+  });
+}
---
+++ src/app/api/route.js
@@ -0,0 +1,18 @@
+export const feedback = [
+  {
+    id: 1,
+    message: "food is good quality.",
+  },
+  {
+    id: 2,
+    message: "food is not bad , but not very good.",
+  },
+  
+];
+
+export async function GET(request) {
+  return Response.json({
+    status: 200,
+    message: "foodNest is runing",
+  });
+}
