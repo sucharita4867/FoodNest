0e17a5473d3037311d58288af7e1b998eebd85ff
sucharita4867 <sucharitasardar7@gmail.com>
2025-12-24 20:50:20 +0530

    adde api connect post get and update

---
+++ src/app/api/feedback/[id]/route.js
@@ -0,0 +1,8 @@
+import { feedback } from "../../route";
+
+export async function GET(request, { params }) {
+  const { id } = await params;
+
+  const singleFeedback = feedback.find((fd) => fd.id == id) || {};
+  return Response.json({ singleFeedback });
+}
