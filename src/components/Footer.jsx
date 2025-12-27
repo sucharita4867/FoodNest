6426dfe5981629007b04542dffd31571c1714c7d
sucharita4867 <sucharitasardar7@gmail.com>
2025-12-27 23:34:37 +0530

    added this changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
     <footer className="footer footer-horizontal footer-center mt-8   rounded p-10">
       <nav className="grid grid-flow-col gap-4">
         <a className="link link-hover">About us</a>
+        <a className="link link-hover">About us</a>
         <a className="link link-hover">Contact</a>
         <a className="link link-hover">Jobs</a>
       </nav>
